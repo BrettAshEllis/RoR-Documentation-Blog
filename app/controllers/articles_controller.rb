d73fa07531ba8308ee0bdb1d08a3cb30bd6134da
Brett Ellis <brett.ash.ellis@gmail.com>
2023-12-13 03:22:56 -0600

    added private strong params + notes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "...", body: "..." )
+    @article = Article.new(article_params)
     
     if @article.save
       redirect_to @article
@@ -20,4 +20,11 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+      # a private method is a method that can only be called from within the class in which it is defined. In this case, the article_params method can only be called from within the ArticlesController class.
+      # params.require(:article) returns an instance of ActionController::Parameters with the name of the model as the key. This is a security feature of Rails. It ensures that only permitted attributes can be used to update our models.
+    end
 end
