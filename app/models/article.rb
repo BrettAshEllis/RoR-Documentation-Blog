36096ab5c374e1ee34ee0188ad07b378871a8504
Brett Ellis <brett.ash.ellis@gmail.com>
2023-12-18 16:09:33 -0600

    added status validation for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,12 @@ class Article < ApplicationRecord
   # validations are used to ensure that only valid data is saved into your database. For example, it may be important to your application to ensure that every user provides a valid email address and mailing address. Rails makes this easy with validations. Validations are typically defined in your model classes.
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10}
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUSES}
+
+  def archived?
+    status == 'archived'
+  end
 end
