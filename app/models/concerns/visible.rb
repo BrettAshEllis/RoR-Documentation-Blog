40799b54ff28fcb7a1eb745b0280e6ddfd49dee5
Brett Ellis <brett.ash.ellis@gmail.com>
2023-12-18 16:37:14 -0600

    added Class methods to conern ( + article count)

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -6,7 +6,13 @@ module Visible
   include do
     validates :status, inclusion: { in: VALID_STATUSES }
   end
-  
+
+  class_methods do
+    def public_count
+      where(status: 'public').count
+    end
+  end
+
   def archived?
     status == 'archived'
   end
