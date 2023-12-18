a0cbff8aa34d3fe13d0e4b9330ddd56844e618e6
Brett Ellis <brett.ash.ellis@gmail.com>
2023-12-18 17:19:26 -0600

    added destroy method to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,17 @@ class CommentsController < ApplicationController
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article) # redirect to the show action of the article controller
 end
-# Because of nesting, we have to get the article object from the article_id parameter for comments to keep track of which article they're attached to.
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other
+  end
+
 private
   def comment_params
     params.require(:comment).permit(:commenter, :body, :status)
   end
 end
+# Because of nesting, we have to get the article object from the article_id parameter for comments to keep track of which article they're attached to.
