06b2a8a86b75ff960bb9500cf70910b97ea9be4b
Brett Ellis <brett.ash.ellis@gmail.com>
2023-12-18 16:04:12 -0600

    Permitted :status key as part of the strong param

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -46,7 +46,7 @@ class ArticlesController < ApplicationController
   end
   private
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :status)
       # a private method is a method that can only be called from within the class in which it is defined. In this case, the article_params method can only be called from within the ArticlesController class.
       # params.require(:article) returns an instance of ActionController::Parameters with the name of the model as the key. This is a security feature of Rails. It ensures that only permitted attributes can be used to update our models.
     end
