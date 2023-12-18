e7fb92153401ca7e124fb938e4d8fb4e03bffe96
Brett Ellis <brett.ash.ellis@gmail.com>
2023-12-18 12:56:35 -0600

    fixed typo

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  mas_many :comments
+  has_many :comments
   # validations are used to ensure that only valid data is saved into your database. For example, it may be important to your application to ensure that every user provides a valid email address and mailing address. Rails makes this easy with validations. Validations are typically defined in your model classes.
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10}
